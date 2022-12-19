d827d1119baf8615c42a26dec16782d071c86bf7
Eduardo <ewang@bionexo.com>
2022-12-19 16:02:32 -0300

    Upload base project

--- display_sqs_message/print_sqs_message.py
+++ display_sqs_message/print_sqs_message.py
@@ -8,28 +8,60 @@ app = flask.Flask(__name__)
 # aws sqs credentials 
 
 # get the url of the queue you wish to paginate
-url = "https://sqs.eu-west-2.amazonaws.com/account-id/queue-name"
+url = "https://sqs.us-west-2.amazonaws.com/055139393900/SQS_TESTE_WANG"
 
 # set the initial page size
 page_size = 10   
-
+client = boto3.client('sqs')
 # the service endpoint for retrieving messages from the queue
-@app.route("/messages", methods=['GET'])
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route("/receive", methods=['GET'])
 def get_messages():
-   
-    # retrieve the message using the sqs method receive messages 
-    response = sqsClient.receive_message(
+  response = client.receive_message(
       QueueUrl=url,
-      MaxNumberOfMessages=page_size
+      AttributeNames=[
+          'SentTimestamp'
+      ],
+      MaxNumberOfMessages=1,
+      MessageAttributeNames=[
+          'All'
+      ],
+      VisibilityTimeout=0,
+      WaitTimeSeconds=0
+  )
+
+  message = response['Messages'][0]
+  receipt_handle = message['ReceiptHandle']
+  return message
+
+@app.route("/write", methods=['GET'])
+def write_message():
+    response = client.send_message(
+      QueueUrl=url,
+      DelaySeconds=10,
+      MessageAttributes={
+        'Title': {
+            'DataType': 'String',
+            'StringValue': 'The Whistler'
+        },
+        'Author': {
+            'DataType': 'String',
+            'StringValue': 'John Grisham'
+        },
+        'WeeksOn': {
+            'DataType': 'Number',
+            'StringValue': '6'
+        }
+      },
+      MessageBody=(
+        'Information about current NY Times fiction bestseller for '
+        'week of 12/11/2016.'
+      )
     )
-   
-    # response from sqs is json
-    messages = response['Messages']
+    return response
 
-    # format of each message is as follows
-    messages_formatted = []
-    for message in messages:
-        messages_formatted.append(json.loads(message['Body']))
-
-    # return the response
-    return json.dumps({'messages': messages_formatted})
+if __name__ == '__main__':
+    app.run(debug=True)
